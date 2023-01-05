ed99979010fb074171ef258bd83813eb0a0d9ebc
426194 <115144024+426194@users.noreply.github.com>
2023-01-05 16:40:10 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,45 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.acceleration(Dimension.X))
+    if (x > 0) {
+        y3 = x - 1
+    }
+    led.plot(x, y)
+    led.unplot(x + 1, y)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(input.acceleration(Dimension.Z))
+    x2 = x
+    y2 = y
+    for (let index = 0; index < 5; index++) {
+        y2 = y2 - 1
+        led.plot(x2, y2)
+        led.unplot(x2, y2 + 1)
+        led.plot(x, y)
+        basic.pause(50)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(input.acceleration(Dimension.Y))
+    if (x < 4) {
+        x = x + 1
+    }
+    led.plot(x, y)
+    led.unplot(x - 1, y)
 })
-let x = 2
-let y = 2
+let y2 = 0
+let x2 = 0
+let x3 = 0
+let y = 0
+let x = 0
+let y3 = 0
+y3 = 0
+y3 = 0
+y3 = 0
+y3 = 0
+y3 = 0
+y3 = 0
 led.plot(x, y)
+led.plot(x3, y3)
 basic.forever(function () {
-	
+    y3 = randint(0, 4)
+    led.plot(x3, y3)
+    basic.pause(2000)
+    led.unplot(x3, y3)
 })
